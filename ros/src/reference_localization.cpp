dc611a9209677f82f2cb3ac5669b7af9188e5cd6
Marc <m.riedlinger@live.de>
2016-08-26 09:40:30 +0200

    Adds missing files

---
+++ ros/src/reference_localization.cpp
@@ -0,0 +1,103 @@
+/*!
+ *****************************************************************
+ * \file
+ *
+ * \note
+ * Copyright (c) 2015 \n
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA) \n\n
+ *
+ *****************************************************************
+ *
+* \note
+* Repository name: squirrel_calibration
+* \note
+* ROS package name: relative_localization
+ *
+ * \author
+ * Author: Marc Riedlinger
+ * \author
+ * Supervised by:
+ *
+ * \date Date of creation: 25.08.2016
+ *
+ * \brief
+ *
+ *
+ *****************************************************************
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer. \n
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution. \n
+ * - Neither the name of the Fraunhofer Institute for Manufacturing
+ * Engineering and Automation (IPA) nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission. \n
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
+
+#include <relative_localization/reference_localization.h>
+#include <relative_localization/visualization_utilities.h>
+#include <relative_localization/relative_localization_utilities.h>
+
+
+ReferenceLocalization::ReferenceLocalization(ros::NodeHandle& nh)
+		: node_handle_(nh)
+{
+	// load parameters
+	std::cout << "\n========== Box Localization Parameters ==========\n";
+	node_handle_.param("update_rate", update_rate_, 0.75);
+	std::cout << "update_rate: " << update_rate_ << std::endl;
+	node_handle_.param<std::string>("child_frame_name", child_frame_name_, "");
+	std::cout << "child_frame_name: " << child_frame_name_ << std::endl;
+	node_handle_.param("wall_length_left", wall_length_left_, 0.75);
+	std::cout << "wall_length_left: " << wall_length_left_ << std::endl;
+	node_handle_.param("wall_length_right", wall_length_right_, 0.75);
+	std::cout << "wall_length_right: " << wall_length_right_ << std::endl;
+
+	// publishers
+	marker_pub_ = node_handle_.advertise<visualization_msgs::Marker>("wall_marker", 1);
+
+	// subscribers
+	laser_scan_sub_ = node_handle_.subscribe("laser_scan_in", 0, &ReferenceLocalization::callback, this);
+
+	// dynamic reconfigure
+	dynamic_reconfigure_server_.setCallback(boost::bind(&ReferenceLocalization::dynamicReconfigureCallback, this, _1, _2));
+	avg_translation_.setZero();
+}
+
+ReferenceLocalization::~ReferenceLocalization()
+{
+}
+
+void ReferenceLocalization::dynamicReconfigureCallback(robotino_calibration::RelativeLocalizationConfig &config, uint32_t level)
+{
+	update_rate_ = config.update_rate;
+	child_frame_name_ = config.child_frame_name;
+	wall_length_left_ = config.wall_length_left;
+	wall_length_right_ = config.wall_length_right;
+	std::cout << "Reconfigure request with\n update_rate=" << update_rate_
+			<< "\n child_frame_name=" << child_frame_name_
+			<< "\n wall_length_left=" << wall_length_left_
+			<< "\n wall_length_right=" << wall_length_right_ << "\n";
+}
